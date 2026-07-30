436ab89227f0540a6623568519d909b06deb8de8
CarlosMtz <charley201050@hotmail.com>
2026-07-30 17:33:46 -0600

    feat(protocol): Definición de constantes en protocolo de comunicación

---
+++ firmware/sensors/src/protocol.h
@@ -0,0 +1,8 @@
+    namespace Sensor {
+        constexpr const char* MAGNET = "magnet";
+    }
+    namespace Pole {
+        constexpr const char* NORTH = "N";
+        constexpr const char* SOUTH = "S";
+        constexpr const char* NONE  = "NONE";
+    }
